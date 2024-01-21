8d8f88f476c6a13168f258463a365e680e4b446e
bhtoan2204 <20127646@student.hcmus.edu.vn>
2024-01-21 22:53:38 +0700

    Set up Prisma and Database for Auth Service

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { RmqModule } from '@app/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthenticationModule } from './auth/authentication.module';
 
 @Module({
   imports: [
@@ -28,9 +27,7 @@ import { JwtModule } from '@nestjs/jwt';
       }),
       inject: [ConfigService]
     }),
-    RmqModule,
+    AuthenticationModule
   ],
-  controllers: [AuthController],
-  providers: [AuthService],
 })
 export class AuthModule { }
--- prisma/migrations/20240118093924_init/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "username" SET DATA TYPE TEXT,
-ALTER COLUMN "password" SET DATA TYPE TEXT;
