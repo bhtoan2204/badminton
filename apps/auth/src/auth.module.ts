429ca70faad55f120c9e710c72fc2695ff169ca0
bhtoan2204 <20127646@student.hcmus.edu.vn>
2024-01-21 17:08:52 +0700

    Auth Service

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,19 +2,33 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { RmqModule } from '@app/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        PORT: Joi.number().default(8080),
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_AUTH_QUEUE: Joi.string().required(),
+        TWILIO_ACCOUNT_SID: Joi.string().required(),
+        TWILIO_AUTH_TOKEN: Joi.string().required(),
       }),
       envFilePath: './apps/auth/.env'
     }),
-    RmqModule
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION')}s`
+        }
+      }),
+      inject: [ConfigService]
+    }),
+    RmqModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
---
+++ apps/auth/src/sms/sms.service.ts
@@ -0,0 +1,9 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TwilioClient } from 'nestjs-twilio';
+
+@Injectable()
+export default class SmsService {
+    client: TwilioClient;
+    
+}
