b51825a31939eb96d2afdf7b3135fbec0d3623c8
Bành Hảo Toàn <20127646@student.hcmus.edu.vn>
2024-02-27 15:58:10 +0700

    Admin authorize

---
+++ apps/gateway/src/admin/admin.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, HttpCode, HttpStatus, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "../auth/guard/jwt-auth.guard";
+import { AdminGuard } from "../auth/guard/authorize/role.guard";
+import { Admin } from "apps/gateway/decorator/role.decorator";
+import { AdminService } from "./admin.service";
+
+@ApiTags('Admin')
+@Controller('admin')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, AdminGuard)
+@Admin(true)
+export class AdminController {
+  constructor(
+    private readonly adminService: AdminService
+  ) { }
+
+  @HttpCode(HttpStatus.OK)
+  @Get('test')
+  async test() {
+    return this.adminService.test();
+  }
+}
