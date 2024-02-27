39ede5a395d1d88079ff95f70f740cbca56e208b
thuhien2105 <thuhien21052002@gmail.com>
2024-02-27 18:18:29 +0700

    CRUD family imcomplete

--- apps/gateway/src/family/family.service.ts
+++ apps/gateway/src/family/family.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
 import { FAMILY_SERVICE } from "apps/gateway/constant/services.constant";
 import { catchError, lastValueFrom, timeout } from "rxjs";
+import { createFamilyDto } from "./dto/createFamilyDto.dto";
 
 @Injectable()
 export class FamilyService {
@@ -9,19 +10,52 @@ export class FamilyService {
     @Inject(FAMILY_SERVICE) private familyClient: ClientProxy
   ) { }
 
-  async getFamily(id_family : number){
-      const response = await this.familyClient.send('family/getFamily', {})
-        .pipe(
-          timeout(5000), 
-          catchError(err => {
-            throw new UnauthorizedException('Unauthorized');
-          })
-        )
+   async getFamily(id: number) {
+    try {
+        const response = await this.familyClient.send('get_Family', {id})
+            .pipe(
+                timeout(5000),
+                catchError(err => {
+                    throw new Error(`Failed to get: ${err.message}`);
+                })
+            );
 
-      const data = await lastValueFrom(response);
-      return data;
+            const data = await lastValueFrom(response);
+            return data;
     } catch (err) {
-      throw err;
+        throw err;
     }
-  
+}
+
+    async createFamily(createFamilyDto: createFamilyDto) {
+      const source = this.familyClient.send('family/create_Family', { createFamilyDto }).pipe(
+          timeout(5000),
+          catchError(err => {
+              throw new Error(`Failed to create family: ${err.message}`);
+          })
+      );;
+      const data = await lastValueFrom(source);
+      return data;
+  }
+
+  async updateFamily(id: number, createFamilyDto: createFamilyDto) {
+    const source = this.familyClient.send('family/update_Family', { id, createFamilyDto }).pipe(
+        timeout(5000),
+        catchError(err => {
+            throw new Error(`Failed to update family: ${err.message}`);
+        })
+    );;
+    const data = await lastValueFrom(source);
+    return data;
+}
+async deleteFamily(id: number) {
+    const source = this.familyClient.send('family/delete_Family', { id }).pipe(
+        timeout(5000),
+        catchError(err => {
+            throw new Error(`Failed to delete family: ${err.message}`);
+        })
+    );;
+    const data = await lastValueFrom(source);
+    return data;
+}
 }
