8d8f19c7f76643f67ad45e29eb02223142574844
thuhien2105 <73421264+thuhien2105@users.noreply.github.com>
2024-03-06 05:09:13 +0700

    create & login user postgre

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -9,22 +9,30 @@ import * as Joi from 'joi';
       ConfigModule.forRoot({
         isGlobal: false,
         validationSchema: Joi.object({
-          DATABASE_URL: Joi.string().required()
+          DB_USERNAME: Joi.string().required(),
+          DB_PASSWORD: Joi.string().required(),
+          DB_HOST: Joi.string().required(),
+          DB_PORT: Joi.number().required(),
+          DB_DATABASE: Joi.string().required()
         }),
         envFilePath: './libs/.env'
       }),
       TypeOrmModule.forRootAsync({
         imports : [ConfigModule],
         useFactory: (configService: ConfigService) => {
-          const dbUrl = configService.get('DATABASE_URL');
-          const connectionOptions = parse(dbUrl);
+          const username = configService.get('DB_USERNAME');
+          const password = configService.get('DB_PASSWORD');
+          const host = configService.get('DB_HOST');
+          const port = configService.get('DB_PORT');
+          const database = configService.get('DB_DATABASE');
+          
           return {
             type: 'postgres',
-            host: connectionOptions.host,
-            port: parseInt(connectionOptions.port),
-            username: connectionOptions.user,
-            password: connectionOptions.password,
-            database: connectionOptions.database,
+            host: host,
+            port: port,
+            username: username,
+            password: password,
+            database: database,
             synchronize: true,
             autoLoadEntities: true,
             ssl: true
