92a2ad45690e55f9bb84031d948c7231004d3cf7
thuhien2105 <73421264+thuhien2105@users.noreply.github.com>
2024-03-06 20:28:28 +0700

    update

--- apps/auth/src/auth/user/user.service.ts
+++ apps/auth/src/auth/user/user.service.ts
@@ -1,15 +1,16 @@
-import {HttpStatus, Injectable } from "@nestjs/common";
-import { InjectRepository, TypeOrmModule } from "@nestjs/typeorm";
+import { HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
 import { Repository, EntityManager } from "typeorm";
 import { CreateAccountDto } from "./dto/createAccount.dto";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import * as bcrypt from 'bcryptjs';
-import { LoginType, Users } from "@app/common";
+import { ConfigService } from "@nestjs/config";
+import { DeleteFileRequest, LoginType, UploadFileRequest, Users } from "@app/common";
 import { RpcException } from "@nestjs/microservices";
-import { parse } from "pg-connection-string";
+import { StorageService } from "../../storage/storage.service";
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
+  storageService: any;
   constructor(
     @InjectRepository(Users) private userRepository: Repository<Users>,
     private readonly configService: ConfigService,
@@ -163,4 +164,95 @@ export class UserService {
       }
     }
   }
+  async updateProfile(user: any, data: any) {
+    try {
+      const { firstname, lastname } = data;
+      if (user.firstname === firstname && user.lastname === lastname) {
+        throw new RpcException({
+          message: 'No changes detected',
+          statusCode: HttpStatus.BAD_REQUEST
+        });
+      }
+      let query, parameters;
+      if (firstname && !lastname) {
+        query = 'SELECT * FROM f_change_firstname($1, $2)';
+        parameters = [user.id_user, firstname];
+      }
+      else if (!firstname && lastname) {
+        query = 'SELECT * FROM f_change_lastname($1, $2)';
+        parameters = [user.id_user, lastname];
+      }
+      else {
+        query = 'SELECT * FROM f_change_firstname_lastname($1, $2, $3)';
+        parameters = [user.id_user, firstname, lastname];
+      }
+      const result = await this.entityManager.query(query, parameters);
+      return result;
+    }
+    catch (error) {
+      throw new RpcException({
+        message: error.message,
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR
+      });
+    }
+  }
+  async changeAvatar(data: any) {
+    try {
+      const { currentUser, file } = data;
+      const filename = 'avatar_' + currentUser.id_user + '_' + Date.now() + '_' + file.originalname;
+      const params: UploadFileRequest = {
+        fileName: filename,
+        file: new Uint8Array(file.buffer.data)
+      }
+      const uploadImageData = await this.storageService.uploadFile(params);
+      const fileUrl = uploadImageData.fileUrl;
+      if (currentUser.avatar) {
+        const deleteParams: DeleteFileRequest = {
+          fileName: (currentUser.avatar).split('/').pop()
+        }
+        await this.storageService.deleteFile(deleteParams);
+      }
+      const query = 'SELECT * FROM f_update_user_avatar($1, $2)';
+      const parameters = [currentUser.id_user, fileUrl];
+      const result = await this.entityManager.query(query, parameters);
+      return {
+        message: file.size + ' bytes uploaded successfully',
+        data: uploadImageData,
+        result: result
+      };
+    }
+    catch (error) {
+      console.log(error.message);
+      throw new RpcException({
+        message: error.message,
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR
+      });
+    }
+  }
+  async validateEmail(dto: any) {
+    try {
+      const { currentUser, data } = dto;
+      const { email, otp } = data;
+      const query = 'SELECT * FROM f_validate_otp($1, $2, $3)';
+      const parameters = [currentUser.id_user, otp, email];
+      const result = await this.entityManager.query(query, parameters);
+      if (!result[0].f_validate_otp) {
+        throw new RpcException({
+          message: 'OTP is not valid',
+          statusCode: HttpStatus.BAD_REQUEST
+        });
+      }
+      return {
+        message: 'Email has been verified',
+        data: result
+      };
+    }
+    catch (error) {
+      throw new RpcException({
+        message: error.message,
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR
+      });
+    }
+  }
+
 }
