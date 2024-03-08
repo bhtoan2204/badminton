42cd968938fdc16763c6e1162bbde6f89dbb83b0
Bành Hảo Toàn <20127646@student.hcmus.edu.vn>
2024-03-08 15:20:28 +0700

    Sonar fix

--- apps/chat/src/chat.service.ts
+++ apps/chat/src/chat.service.ts
@@ -1,5 +1,6 @@
 import { Message, MessageDocument } from '@app/common';
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -7,28 +8,44 @@ import { Model } from 'mongoose';
 export class ChatService {
   constructor(
     @InjectModel(Message.name) private messageRepository: Model<MessageDocument>,
-  ) {}
+  ) { }
 
   async getMessages(senderId: string, receiverId: string, index: number): Promise<Message[]> {
-    const limit = 50; 
-    const skip = index * limit; 
-    return this.messageRepository.find({
-      $or: [
-        { senderId: senderId, receiverId: receiverId },
-        { senderId: receiverId, receiverId: senderId }
-      ]
-    }).sort({ timestamp: -1 }) 
-      .skip(skip)
-      .limit(limit);
+    try {
+      const limit = 50;
+      const skip = index * limit;
+      return this.messageRepository.find({
+        $or: [
+          { senderId: senderId, receiverId: receiverId },
+          { senderId: receiverId, receiverId: senderId }
+        ]
+      }).sort({ timestamp: -1 })
+        .skip(skip)
+        .limit(limit);
+    }
+    catch (error) {
+      throw new RpcException({
+        message: error.message,
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR
+      });
+    }
   }
-  
+
   async saveMessage(idUser: string, messageData: { message: string; receiverId: string; }): Promise<Message> {
-    const newMessage = new this.messageRepository({
-      senderId: idUser,
-      receiverId: messageData.receiverId,
-      content: messageData.message,
-      timestamp: new Date(),
-    });
-    return newMessage.save();
+    try {
+      const newMessage = new this.messageRepository({
+        senderId: idUser,
+        receiverId: messageData.receiverId,
+        content: messageData.message,
+        timestamp: new Date(),
+      });
+      return newMessage.save();
+    }
+    catch (error) {
+      throw new RpcException({
+        message: error.message,
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR
+      });
+    }
   }
 }
