5e3d13e7ea46f6a6a5c7195e9dd736ac16304572
Bành Hảo Toàn <20127646@student.hcmus.edu.vn>
2024-03-10 19:30:04 +0700

    chat person to person

--- apps/gateway/src/chat/chat.controller.ts
+++ apps/gateway/src/chat/chat.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, HttpCode, HttpStatus, Param, UseGuards } from "@nestjs/common";
-import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "../auth/guard/jwt-auth.guard";
 import { CurrentUser } from "../utils/decorator/current-user.decorator";
 import { ChatService } from "./chat.service";
@@ -16,7 +16,9 @@ export class ChatController {
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Get messages' })
   @Get('getMessages/:id_user/:index')
-  async getMessages(@CurrentUser() user, @Param() id_user: string, @Param() index: number) {
+  @ApiParam({ name: 'id_user', required: true, description: 'The ID of the other user' })
+  @ApiParam({ name: 'index', required: true, description: 'Pagination index', type: Number })
+  async getMessages(@CurrentUser() user, @Param('id_user') id_user: string, @Param('index') index: number) {
     return this.chatService.getMessages(user.id_user, id_user, index);
   }
 }
--- apps/gateway/src/chat/chat.service.ts
+++ apps/gateway/src/chat/chat.service.ts
@@ -6,11 +6,12 @@ import { lastValueFrom, timeout } from 'rxjs';
 @Injectable()
 export class ChatService {
   constructor(
-    @Inject(CHAT_SERVICE) private readonly chatClient : ClientProxy
-  ) {}
+    @Inject(CHAT_SERVICE) private readonly chatClient: ClientProxy
+  ) { }
 
-  async getMessages(sender_id, receiver_id, index) {
+  async getMessages(sender_id: string, receiver_id: string, index: number) {
     try {
+      console.log({ sender_id, receiver_id, index });
       const response = this.chatClient.send('chatClient/getMessages', { sender_id, receiver_id, index }).pipe(timeout(5000));
       return await lastValueFrom(response);
     }
@@ -19,7 +20,7 @@ export class ChatService {
     }
   }
 
-  async saveMessage(id_user, message) {
+  async saveMessage(id_user: string, message: any) {
     try {
       const response = this.chatClient.send('chatClient/sendMessage', { id_user, message });
       return await lastValueFrom(response);
