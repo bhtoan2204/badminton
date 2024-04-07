5b9b13f519f0870c11290999004ffe8201dff8e7
Bành Hảo Toàn <20127646@student.hcmus.edu.vn>
2024-04-07 19:04:00 +0700

    Create and Get education and subject

--- apps/gateway/src/education/education.controller.ts
+++ apps/gateway/src/education/education.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Put, UseGuards } from "@nestjs/common";
-import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Put, Query, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "../auth/guard/jwt-auth.guard";
 import { CurrentUser } from "../utils";
 import { EducationService } from "./education.service";
@@ -23,15 +23,24 @@ export class EducationController {
 
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Get all education progress' })
+  @ApiQuery({ name: 'page', required: false, type: String })
+  @ApiQuery({ name: 'itemsPerPage', required: false, type: String })
+  @ApiQuery({ name: 'id_family', required: true, type: Number })
   @Get('getAll')
-  async getAllEducationProgress(@CurrentUser() currentUser) {
-
+  async getAllEducationProgress(@CurrentUser() currentUser, @Query('page') page, @Query('itemsPerPage') itemsPerPage, @Query('id_family') id_family) {
+    const pageNumber = parseInt(page, 10) || 1;
+    const itemsPerPageNumber = parseInt(itemsPerPage, 10) || 10;
+    return this.educationService.getAllEducationProgress(currentUser.id_user, pageNumber, itemsPerPageNumber, id_family);
+    
   }
 
   @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Get all education progress' })
-  @Get('getDetail/:id_education_progress')
-  async getDetailEducationProgress(@CurrentUser() currentUser) {
+  @ApiParam({ name: 'id_family', required: true, type: Number })
+  @ApiParam({ name: 'id_education_progress', required: true, type: Number })
+  @Get('getDetail/:id_family/:id_education_progress')
+  async getDetailEducationProgress(@CurrentUser() currentUser, @Param('id_family') id_family, @Param('id_education_progress') id_education_progress) {
+    return this.educationService.getDetailEducationProgress(currentUser.id_user, id_family, id_education_progress);
 
   }
 
