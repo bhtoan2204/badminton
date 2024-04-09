a9290466279f23db20a2e4def618eb83cc3a372c
Bành Hảo Toàn <20127646@student.hcmus.edu.vn>
2024-04-09 16:36:17 +0700

    successful

---
+++ apps/gateway/src/education/subjects/dto/addComponentScore.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
+
+export class AddComponentScoreDto {
+  @ApiProperty({ description: "Subject id", example: 1 })
+  @IsNumber()
+  @IsNotEmpty()
+  id_subject: number
+  
+  @ApiProperty({ description: "Education progress id", example: 1 })
+  @IsNumber()
+  @IsNotEmpty()
+  id_education_progress: number
+
+  @ApiProperty({ description: "Family id", example: 1 })
+  @IsNumber()
+  @IsNotEmpty()
+  id_family: number
+
+  @ApiProperty({ description: "Name of component score", example: "Homework 1" })
+  @IsString()
+  @IsNotEmpty()
+  component_name: string
+
+  @ApiProperty({ description: "Score of component", example: 8.9 })
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0.0, { message: 'Score must be at least 0.0' })
+  @Max(10.0, { message: 'Score must not be greater than 10.0' })
+  score: number
+}
---
+++ apps/gateway/src/education/subjects/dto/changeStatusSubject.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from "@nestjs/swagger"
+import { IsEnum, IsNotEmpty, IsNumber } from "class-validator"
+
+enum Status {
+  IN_PROGRESS = 'in_progress',
+  DONE = 'done'
+}
+
+export class ChangeStatusSubjectDto {
+  @ApiProperty({ description: 'Subject id' })
+  @IsNumber()
+  @IsNotEmpty()
+  id_subject: number
+
+  @ApiProperty({ description: 'Education progress id'})
+  @IsNumber()
+  @IsNotEmpty()
+  id_education_progress: number
+
+  @ApiProperty({ description: 'Family id'})
+  @IsNumber()
+  @IsNotEmpty()
+  id_family: number
+
+  @ApiProperty({ description: 'Status' })
+  @IsEnum(Status, {
+    message: `Status must be either ${Status.IN_PROGRESS} or ${Status.DONE}`
+  })
+  @IsNotEmpty()
+  status: string
+}
