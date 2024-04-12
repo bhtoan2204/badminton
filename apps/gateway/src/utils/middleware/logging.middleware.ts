f86610f3df4b6efe5ea7f91b0628b3f3fc5b0589
Bành Hảo Toàn <20127646@student.hcmus.edu.vn>
2024-04-12 15:18:21 +0700

    clear logs, resize persistent chain volume

--- apps/gateway/src/utils/middleware/logging.middleware.ts
+++ apps/gateway/src/utils/middleware/logging.middleware.ts
@@ -11,18 +11,25 @@ export class AppLoggerMiddleware implements NestMiddleware {
 
     response.on('finish', () => {
       const { statusCode } = response;
-      const contentLength = response.get('content-length') || 0;
+      const contentLength = response.get('content-length') || '0';
       const [sec, nanosec] = process.hrtime(start);
       const responseTime = (sec * 1000 + nanosec / 1e6).toFixed(0);
+      const errorMessage = response.statusMessage || 'No error message provided';
+
+      const logEntry = {
+        ip: ip,
+        method: method,
+        url: url,
+        statusCode: statusCode,
+        contentLength: contentLength,
+        responseTimeMs: responseTime,
+        message: `${statusCode >= 400 ? `${errorMessage}` : 'Successful'}`
+      };
 
       if (statusCode >= 400) {
-        logger.error(
-          `Error in ${method} ${url}: ${statusCode} ${contentLength} - ${responseTime} ms - ${ip}`
-        );
+        logger.error(logEntry);
       } else {
-        logger.info(
-          `Successful in ${method} ${url} ${statusCode} ${contentLength} - ${responseTime} ms - ${ip}`
-        );
+        logger.info(logEntry);
       }
     });
 
