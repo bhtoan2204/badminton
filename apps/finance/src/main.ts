f2e008f8075c89fa3b066435660bbfcb9ca1e874
Bành Hảo Toàn <20127646@student.hcmus.edu.vn>
2024-04-22 09:39:09 +0700

    finance fix

--- apps/finance/src/main.ts
+++ apps/finance/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { FinanceModule } from './finance.module';
+import { RmqService } from '@app/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(FinanceModule);
-  await app.listen(3000);
+  const rmqService = app.get<RmqService>(RmqService);
+  app.connectMicroservice(rmqService.getOptions('FINANCE'));
+  app.startAllMicroservices();
+  await app.init();
 }
 bootstrap();
