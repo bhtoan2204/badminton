ddc8cd687bd82964e873e4fa1d70776fc0fec5fe
Bành Hảo Toàn <20127646@student.hcmus.edu.vn>
2024-06-06 01:37:15 +0700

    utilities svc

--- apps/gateway/src/admin/datafetcher/datafetcher.controller.ts
+++ apps/gateway/src/admin/datafetcher/datafetcher.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, HttpCode, HttpStatus, Param, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, UseGuards } from "@nestjs/common";
 import { DatafetcherService } from "./datafetcher.service";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { Admin, AdminGuard, JwtAuthGuard } from "../../utils";
+import { GetListOrdersDto } from "./dto/getListOrders.dto";
 
 @ApiTags('Admin Datafetcher')
 @Controller()
@@ -13,21 +14,31 @@ export class DatafetcherController {
     private readonly datafetcherService: DatafetcherService
   ) { }
 
+  @ApiOperation({ summary: 'Get IP data' })
   @HttpCode(HttpStatus.OK)
   @Get('ipData/:ip')
   async getIpData(@Param('ip') ip: string) {
     return this.datafetcherService.getIpData(ip);
   }
 
+  @ApiOperation({ summary: 'Get summary' })
   @HttpCode(HttpStatus.OK)
   @Get('summary')
   async getSummary() {
     return this.datafetcherService.getSummary();
   }
 
+  @ApiOperation({ summary: 'Get revenue last 6 months' })
   @HttpCode(HttpStatus.OK)
   @Get('revenueLast6Months')
   async getRevenueLast6Months() {
     return this.datafetcherService.getRevenueLast6Months();
+  }
+
+  @ApiOperation({ summary: 'Get list orders' })
+  @HttpCode(HttpStatus.OK)
+  @Post('listOrders')
+  async getListOrders(@Body() body: GetListOrdersDto) {
+    return this.datafetcherService.getListOrders(body);
   }
 }
