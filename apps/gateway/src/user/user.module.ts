bcb69135270959fdf7dbe865eb1a1c19cf58ea5b
Bành Hảo Toàn <20127646@student.hcmus.edu.vn>
2024-06-12 21:04:24 +0700

    setup

--- apps/gateway/src/user/user.module.ts
+++ apps/gateway/src/user/user.module.ts
@@ -3,10 +3,23 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { USER_SERVICE } from '../utils';
-
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
-  imports: [RmqModule.register({ name: USER_SERVICE })],
+  imports: [
+    RmqModule.register({ name: USER_SERVICE }),
+    ThrottlerModule.forRoot([{
+      ttl: 300,
+      limit: 1,
+    }]),
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class UserModule {}
