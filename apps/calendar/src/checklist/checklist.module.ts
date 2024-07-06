9dd5dc92071aba5c17033c30e8b0cb7a232c6884
Bành Hảo Toàn <20127646@student.hcmus.edu.vn>
2024-07-06 10:36:19 +0700

    fix bug

--- apps/calendar/src/checklist/checklist.module.ts
+++ apps/calendar/src/checklist/checklist.module.ts
@@ -3,12 +3,13 @@ import { CalendarModule } from '../calendar.module';
 import { ChecklistController } from './checklist.controller';
 import { ChecklistService } from './checklist.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Checklist, ChecklistType } from '@app/common';
+import { Checklist, ChecklistType, DatabaseModule } from '@app/common';
 
 @Module({
   imports: [
     forwardRef(() => CalendarModule),
-    TypeOrmModule.forFeature([Checklist]),
+    DatabaseModule,
+    TypeOrmModule.forFeature([Checklist, ChecklistType]),
   ],
   controllers: [ChecklistController, ChecklistType],
   providers: [ChecklistService],
