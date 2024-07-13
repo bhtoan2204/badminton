991e482367976d3e1fedad0ed59b77dd6494eed0
Bành Hảo Toàn <20127646@student.hcmus.edu.vn>
2024-07-14 02:27:13 +0700

    cache jwt

--- apps/gateway/src/utils/strategies/jwt.strategy.ts
+++ apps/gateway/src/utils/strategies/jwt.strategy.ts
@@ -5,12 +5,15 @@ import { ClientProxy } from '@nestjs/microservices';
 import { lastValueFrom, timeout } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
 import { AUTH_SERVICE } from '../../utils';
+import { InjectRedis } from '@nestjs-modules/ioredis';
+import Redis from 'ioredis';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(
     @Inject(AUTH_SERVICE) private authClient: ClientProxy,
     private readonly configService: ConfigService,
+    @InjectRedis() private readonly redisService: Redis,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -24,11 +27,18 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   private async validateUser(payload: any) {
+    const cacheKey = `user:${payload.id_user}`;
     try {
+      const cachedUser = await this.redisService.get(cacheKey);
+      if (cachedUser) {
+        return JSON.parse(cachedUser);
+      }
       const userRequest$ = this.authClient
         .send('authClient/validateUserId', payload.id_user)
         .pipe(timeout(15000));
-      return await lastValueFrom(userRequest$);
+      const user = await lastValueFrom(userRequest$);
+      await this.redisService.set(cacheKey, JSON.stringify(user), 'EX', 3600); // Cache for 1 hour
+      return user;
     } catch (err) {
       throw new UnauthorizedException(err.message);
     }
